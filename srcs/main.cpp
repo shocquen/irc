6bbd1bed47ed0c9d62c61b46b44bc5a0f5ebda42
Souria-Saky HOCQUENGHEM <shocquen@student.42.fr>
2023-12-04 15:21:15 +0100

    feat: A very basic simple version craply writed of a server

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -5,80 +5,112 @@
 #include <poll.h>
 #include <stdio.h>
 #include <string>
+#include <strings.h>
 #include <sys/poll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <vector>
 
-#define PORT 4242
+#define PORT 8080
 #define TIMEOUTMS 500
-#define MAX_FD 2
 #define BUFFER_SIZE 2024
 #define MAX_LISTEN 10
 
 int main(void) {
-  struct pollfd pfds[MAX_FD];
-  nfds_t nfds = 1; // The serv has min a socket
+  // struct pollfd pfds[MAX_FD];
+  // nfds_t nfds = 1; // The serv has min a socket
+  std::vector<struct pollfd> vfds;
 
 /* ------------------------------------------------------------------------- */
 /* Bind 1st socket to server                                                 */
 /* ------------------------------------------------------------------------- */
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
+  int serverFd = socket(AF_INET, SOCK_STREAM, 0);
+  if (serverFd < 0) {
     perror("socket failed");
     exit(EXIT_FAILURE);
   }
 
   int opt = 1;
-  struct sockaddr_in address;
-  socklen_t addrlen = sizeof(address);
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+  struct sockaddr_in serverAddress;
+  if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
                  sizeof(opt))) {
     perror("setsockopt");
     exit(EXIT_FAILURE);
   }
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(PORT);
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_addr.s_addr = INADDR_ANY;
+  serverAddress.sin_port = htons(PORT);
 
-  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+  if (bind(serverFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
     perror("bind failed");
     exit(EXIT_FAILURE);
   }
-/* ------------------------------------------------------------------------- */
-
-	if (listen(server_fd, 3) < 0) {
+	if (listen(serverFd, 3) < 0) {
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
 
-  // Now we need PAUL! 
+/* ------------------------------------------------------------------------- */
+
+  // Here Paul is at work!
   int ready;
   ssize_t s;
-  char buf[10];
+  char buf[BUFFER_SIZE];
+  int newSocketFd;
+  struct sockaddr_in clientAddress;
+  socklen_t addrlen = sizeof(clientAddress);
   while (true) {
-    printf("About to poll()\n");
+    nfds_t nfds = vfds.size() + 1;
+    struct pollfd pfds[nfds];
+
+    pfds[0].fd = serverFd;
+    pfds[0].events = POLLIN;
+    for (nfds_t i = 1; i < nfds; i++) {
+      pfds[i] = vfds[i - 1];
+    }
+
+    // printf("About to poll()\n");
     ready = poll(pfds, nfds, -1);
     if ( ready == -1) {
       perror("poll");
       exit(EXIT_FAILURE);
     }
-    printf("Ready: %d\n", ready);
-
+    // printf("Ready: %d\n", ready);
     for (nfds_t i = 0; i < nfds; i++) {
-      if (pfds[i].events & POLLIN) {
-        s = read(pfds[i].fd, buf, sizeof(buf));
+      if (!(pfds[i].revents & POLLIN)) continue ;
+
+      if (pfds[i].fd == serverFd) {
+      	if ((newSocketFd = accept(serverFd, (struct sockaddr*)&clientAddress, &addrlen)) < 0) {
+      		perror("accept");
+      		exit(EXIT_FAILURE);
+      	} else {
+          struct pollfd tmp;
+          tmp.fd = newSocketFd;
+          tmp.events = POLLIN | POLLOUT;
+          vfds.push_back(tmp);
+        }
+      } else {
+        bzero(buf, BUFFER_SIZE);
+        printf("About to recv()\n");
+        s = recv(pfds[i].fd, buf, BUFFER_SIZE, MSG_DONTWAIT);
+        printf("recv() is a succes\n");
         if ( s == -1) {
           perror("read");
           exit(EXIT_FAILURE);
         }
-        printf("\tread %zd bytes: %.*s\n", s, (int)s, buf);
-      } else { // POLLERR | POLLHUP
-        printf("\tclosing fd %d\n", pfds[i].fd);
-        if (close(pfds[i].fd) == -1) {
-          perror("close");
-          exit(EXIT_FAILURE);
+        if (s == 0 ) {
+          printf("\tClosing fd %d\n", pfds[i].fd);
+          close(pfds[i].fd);
+          for (std::vector<struct pollfd>::iterator it = vfds.begin(); it != vfds.end(); it++) {
+            if (it->fd == pfds[i].fd) {
+              printf("\tEreasing fd %d\n", pfds[i].fd);
+              vfds.erase(it);
+              break ;
+            }
+          }
         }
+        printf("\tread from %d, %zd bytes: %.*s\n", pfds[i].fd, s, (int)s, buf);
       }
     }
   }
